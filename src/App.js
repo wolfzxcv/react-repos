d4e54de9d9d381bb9cf66b0465809e4f28973f07
Nien <wolfzxcv@gmail.com>
2019-06-12 22:17:28 +0100

    basic

--- src/App.js
+++ src/App.js
@@ -3,22 +3,42 @@ import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
 const App = ({className}) => {
-  const [height, setHeight] = useState('')
-  const [weight, setWeight] = useState('')
+  const [input, setInput] = useState({height:'',weight:''})
+ // const [display, setDisplay] = useState([])
+ 
+  const calcu = () => {
+   if( input.height<250 && input.height>90 && input.weight<300 && input.weight>10 ){
+    setInput(input)
+    const bmi = (input.weight/input.height*100/input.height*100).toFixed(2)    
+    runResult(input.height, input.weight, bmi)
+  }
+    else{
+    alert('please enter correct number')}   
+  }
+
+  const runResult = (height, weight, bmi) =>{
+    const date = new Date().toLocaleDateString()
+    alert(`height:${height} weight:${weight} bmi:${bmi} date:${date}`)
+  }
 
   return (
     <div className={className}>
-    <div><img src='./BMICLogo.png' alt='BMICLogo' /></div>  
+    <header>  
+    <div className='logo'>BMI</div>  
 
     <div>
     <p>Your height / cm</p>  
-    <input type='text' placeholder='Enter your height' value={height} onChange={e=>setHeight(e.target.value)} />
+    <input type='text' placeholder='Enter your height' value={input.height} onChange={e=>setInput({...input, height: e.target.value})} />
     <p>Your weight / kg</p>
-    <input type='text' placeholder='Enter your weight' value={weight} onChange={e=>setWeight(e.target.value)} />
+    <input type='text' placeholder='Enter your weight' value={input.weight} onChange={e=>setInput({...input, weight: e.target.value})} />
     </div>
 
-    <div>Get the result</div>
-    
+    <div className='circle-result' onClick={calcu}>Get your result</div>
+    </header>
+
+    <div className='bmi-title'>
+     BMI result
+    </div>  
     </div>
   )
 }
@@ -26,12 +46,82 @@ const App = ({className}) => {
 App.propTypes = {
   className: PropTypes.string
 }
-
+ 
 const StyledApp = styled(App)`
+ 
+ display: flex;
+ flex-direction: column;
+ font-family: cursive;
+
+header{
   display: flex;
-  justify-content:space-around;
+  justify-content: space-evenly;
   align-items: center;
   text-align: center;
+  width: 100vw;
+  height: 300px;
+  background-color: #424242;
+}  
+
+.logo{
+  font-size: 48px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #424242;
+  background-color: #FFD366;
+  width: 120px;
+  height: 120px;
+  border-radius: 20%;
+  border: 1px solid #FFD366;
+  box-shadow:0 1px 2px 2px blue;
+}
+
+p{
+  font-size: 18px;
+  color: #FFD366; 
+}
+
+input {
+ width: 250px;
+ height: 40px;
+ &[type=text]{
+  width:100%;
+  border:2px solid #FFD366;
+  background:rgba(255,255,255,0.18);
+  color:#FFF;
+  outline:0;
+  font-size:24px;
+  padding:2px 12px;
+  border-radius:10px;
+ }
+}
+
+.circle-result{  
+  font-size: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #424242;
+  background-color: #FFD366;
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  border: 1px solid #FFD366;
+  &:hover, &:active{
+  box-shadow:0 1px 6px 3px rgba(255,196,50,0.64);
+  background:rgba(222,168,33,1);
+  }
+}  
+
+.bmi-title{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  font-size: 24px;
+  color: #424242;
+}
 `
 
 StyledApp.displayName = 'App'
