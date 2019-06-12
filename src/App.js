d486bd8d3abac012d172c55a961b9818b686ff15
Nien <wolfzxcv@gmail.com>
2019-06-12 22:17:03 +0100

    basic func

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react' 
+import styled from 'styled-components' 
+import PropTypes from 'prop-types'
 
-function App() {
+const App = ({className}) => {
+  const [height, setHeight] = useState('')
+  const [weight, setWeight] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={className}>
+    <div><img src='./BMICLogo.png' alt='BMICLogo' /></div>  
+
+    <div>
+    <p>Your height / cm</p>  
+    <input type='text' placeholder='Enter your height' value={height} onChange={e=>setHeight(e.target.value)} />
+    <p>Your weight / kg</p>
+    <input type='text' placeholder='Enter your weight' value={weight} onChange={e=>setWeight(e.target.value)} />
     </div>
-  );
+
+    <div>Get the result</div>
+    
+    </div>
+  )
 }
 
-export default App;
+App.propTypes = {
+  className: PropTypes.string
+}
+
+const StyledApp = styled(App)`
+  display: flex;
+  justify-content:space-around;
+  align-items: center;
+  text-align: center;
+`
+
+StyledApp.displayName = 'App'
+
+export default StyledApp
+ 
