451f1ce4b4e67a795163ce8f7519c559a2e629a8
Nien-Ying Chou <wolfzxcv@gmail.com>
2022-04-18 15:01:00 +0800

    refactor(all): upgrade to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
@@ -7,15 +7,17 @@ import { store } from './app/store';
 import * as serviceWorker from './serviceWorker';
 import './styles/index.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(
   <React.StrictMode>
     <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
     </HashRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
